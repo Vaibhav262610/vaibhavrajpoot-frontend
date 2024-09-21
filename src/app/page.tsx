47a5f0a362cf32531556c5f0b90f67e6c378a7c7
Vaibhav262610 <branda56235@gmail.com>
2024-09-21 16:13:44 +0530

    h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client"; // This might not be necessary; see notes below
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Header from "@/components/Header";
@@ -5,15 +8,32 @@ import Navbar from "@/components/Navbar";
 import SideIcons from "@/components/SideIcons";
 import Whyme from "@/components/Whyme";
 import Work from "@/components/Work";
+import NotFound from "@/components/NotFound";
 
-export default function Home() {
+export default function App() {
   return (
-    <>
+    <Router>
       <Navbar />
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/whyme" element={<Whyme />} />
+        <Route path="*" element={<NotFound />} /> 
+      </Routes>
+      <SideIcons />
+    </Router>
+  );
+}
+
+// Home component for the main content
+function Home() {
+  return (
+    <>
       <About />
       <Work />
-      <SideIcons />
       <Whyme />
       <Contact />
     </>
